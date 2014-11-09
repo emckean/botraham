dbabc0a8e0449a62e646a898a0a8193a501df6ed
Erin McKean <erin@wordnik.com>
2014-11-08 16:57:10 -0800

    adding config for heroku

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 // Put your own Twitter App keys here. See README.md for more detail.
 module.exports = {
-  consumer_key:         'YOUR_CONSUMER_KEY',
-  consumer_secret:      'YOUR_CONSUMER_SECRET',
-  access_token:         'YOUR_ACCESS_TOKEN',
-  access_token_secret:  'YOUR_ACCESS_TOKEN_SECRET'
+  consumer_key:         '0hm01VNHGeH1YCbCzhllflo2H',
+  consumer_secret:      'WUnVLx9YstYnvx9TD5I1N57vZgSNYDEEjwEDNYKyg0foHhAeKg',
+  access_token:         '589481140-hp4sSR9K3ThKGKdgQ6DVdcCPs5ug36a6Ske60gHY',
+  access_token_secret:  'sVZ7EOuWL8A4qY8sk3XvxzylAjRzYSdWcDQdyZBwkBChd'
 };
