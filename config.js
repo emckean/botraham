4c246984995a976a3286123f21b1f3dd156f499f
Erin McKean <erin@wordnik.com>
2014-11-08 18:01:48 -0800

    bigger lincoln file

--- config.js
+++
@@ -1,7 +0,0 @@
-// Put your own Twitter App keys here. See README.md for more detail.
-module.exports = {
-  consumer_key:         '0hm01VNHGeH1YCbCzhllflo2H',
-  consumer_secret:      'WUnVLx9YstYnvx9TD5I1N57vZgSNYDEEjwEDNYKyg0foHhAeKg',
-  access_token:         '589481140-hp4sSR9K3ThKGKdgQ6DVdcCPs5ug36a6Ske60gHY',
-  access_token_secret:  'sVZ7EOuWL8A4qY8sk3XvxzylAjRzYSdWcDQdyZBwkBChd'
-};
